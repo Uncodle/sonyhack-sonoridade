2b567f6dbbd96608ff85cdaee5b395487f45fb32
G. Silva Ribeiro <g.silva.ribeiro@avanade.com>
2019-03-24 01:55:21 -0300

    Desenvolvimento da estrutura HTML e rotas iniciais da aplicação

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+
+const appRoutes = [
+   { path: '', component: HomepageComponent },
+   { path: 'dashboard', component: DashboardComponent } 
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    DashboardComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
