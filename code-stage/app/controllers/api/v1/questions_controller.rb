05a84f7223c94ee9b7c5c56a847271b911426fd1
Danielle Alvino <danialvino@MacBook-Pro-de-Danielle.local>
2019-03-24 09:48:30 -0300

    create endpoints

--- code-stage/app/controllers/api/v1/questions_controller.rb
+++ code-stage/app/controllers/api/v1/questions_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::QuestionsController < ActionController::Base
+  protect_from_forgery with: :null_session
 
   def questions_field
     @questions = Question.where(field_id: params[:field_id]).order(:rating)
@@ -8,6 +9,24 @@ class Api::V1::QuestionsController < ActionController::Base
   def questions_musical
     @questions = Question.where(musical_gender_id: params[:field_id]).order(:rating)
     render json: { questions: @questions.as_json(:include => :answers) } 
+  end
+
+  def new_question
+    Question.create(title: params[:title], description: params[:description], 
+    field_id: params[:field_id], musical_gender_id: params[:musical_gender_id])
+    render json: { message: "Pergunta criada" }
+  end
+
+  def persona_question
+    Question.create(title: params[:title], description: params[:description], 
+    field_id: params[:field_id], musical_gender_id: params[:musical_gender_id],
+    persona_id: params[:persona_id])
+    render json: { message: "Pergunta criada" }
+  end
+
+  def my_questions
+    @questions = Question.where(user_id: params[:user_id])
+    render json: { questions: @questions.as_json(:include => :answers) }
   end
 
 end
